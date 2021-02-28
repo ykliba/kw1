ddbb09904db708cec62ac735cbc93f91a2634fb1
ykliba <mediaboxing2@gmail.com>
2021-02-28 11:02:21 +0900

    header login logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :posts
 end
